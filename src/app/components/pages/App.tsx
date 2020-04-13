f49f358837a96b0db171dd9dde7b9af0a34d89d9
Sliip831143 <sliip831143@gmail.com>
2020-04-14 04:01:21 +0900

    ダークモード実装

--- src/app/components/pages/App.tsx
+++ src/app/components/pages/App.tsx
@@ -5,26 +5,26 @@ import ToggleButton from '@material-ui/lab/ToggleButton';
 import styled from 'styled-components';
 
 const App = ({ children }: { children?: any }) => {
-  const [selected, setSelected] = useState(
-    typeof window !== 'undefined' ? window.matchMedia('(prefers-color-scheme: dark)').matches : true
-  );
-  const [nativeDarkModeOn, setNativeDarkModeOn] = useState(false);
+  const [theme, setTheme] = useState('');
+  let darkModeOn;
 
   return (
-    <StyledMain style={{ backgroundColor: `${(typeof window !== 'undefined' ?
-      (nativeDarkModeOn ? nativeDarkModeOn : window.matchMedia('(prefers-color-scheme: dark)').matches) : false
-    ) && selected ? '#364156' : '#FBCC44'}` }}>
+    <StyledMain className={theme}>
       <StyledToggleButton
         value="check"
-        selected={selected}
+        theme={theme}
         onChange={() => {
-          setSelected(!selected);
-          if(typeof window !== 'undefined' && nativeDarkModeOn === false) {
-            setNativeDarkModeOn(true)
-          }
+          setTheme((window.matchMedia('(prefers-color-scheme: dark)').matches === false &&
+            theme === '' || theme === 'light') ? 'dark' : 'light'
+          );
         }}
       >
-        {selected ? <DarkBulbIcon /> : <LightBulbIcon />}
+        {typeof window !== 'undefined' &&
+          (darkModeOn = window.matchMedia('(prefers-color-scheme: dark)').matches,
+            theme === '' ? (darkModeOn ? <DarkBulbIcon /> : <LightBulbIcon />) :
+              (theme === 'light' ? <LightBulbIcon /> : <DarkBulbIcon />)
+          )
+        }
       </StyledToggleButton>
       {children}
     </StyledMain>
@@ -37,6 +37,15 @@ const StyledMain = styled.main`
   width: 100%;
   height: 100%;
   background-color: #FBCC44;
+  @media (prefers-color-scheme: dark) {
+    background-color: #364156;
+  }
+  &.light {
+    background-color: #FBCC44 !important;
+  }
+  &.dark {
+    background-color: #364156 !important;
+  }
 `;
 
 const StyledToggleButton = styled(ToggleButton)`
